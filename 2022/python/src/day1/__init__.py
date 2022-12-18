a271825fbbfd71cfb6563c86874f4e3346585561
Kal Cantrell <klcantrell@sep.com>
2022-12-17 21:49:06 -0500

    [2022/Python/Day1]: complete part 1 and 2

---
+++ 2022/python/src/day1/__init__.py
@@ -0,0 +1,23 @@
+def part1() -> None:
+    print(f'Day 1 Part 1 solution: {max(calories_by_elf())}')
+
+def part2() -> None:
+    calories_by_elf_sorted_descending = sorted(calories_by_elf(), reverse=True)
+    print(f'Day 1 Part 2 solution: {sum(calories_by_elf_sorted_descending[:3])}')
+
+def calories_by_elf() -> list[int]:
+    parsed_data: list[list[int]] = []
+    group: list[int] = []
+
+    with open('src/day1/data.txt') as f:
+        for line in f:
+            if line == "\n":
+                parsed_data.append(group)
+                group = []
+            else:
+                group.append(int(line.strip()))
+
+        if len(group) > 0:
+            parsed_data.append(group)
+
+    return list(map(lambda x: sum(x), parsed_data))
