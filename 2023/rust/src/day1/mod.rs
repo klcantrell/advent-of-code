974fba5fbc5a3c9b3809276ddbc2f4b0b6d71cf8
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-12-01 14:45:32 -0500

    [2023/Rust/Day1] complete part 1

--- 2023/rust/src/day1/mod.rs
+++ 2023/rust/src/day1/mod.rs
@@ -1,3 +1,27 @@
+use crate::utils;
+
 pub fn part_1() {
-    println!("Day 1 Part 1")
+    if let Ok(lines) = utils::read_lines("./puzzle_inputs/day1.txt") {
+        let result: i32 = lines
+            .flatten()
+            .map(|line| {
+                let digits: Vec<i32> = line
+                    .split("")
+                    .filter_map(|character| character.parse::<i32>().ok())
+                    .collect();
+
+                if let (Some(first), Some(last)) = (digits.first(), digits.last()) {
+                    if let Ok(result) = format!("{}{}", first, last).parse::<i32>() {
+                        result
+                    } else {
+                        0
+                    }
+                } else {
+                    0
+                }
+            })
+            .sum();
+
+        println!("Day 1 Part 1 solution: {}", result)
+    }
 }
