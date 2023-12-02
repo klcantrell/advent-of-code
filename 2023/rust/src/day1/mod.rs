2899e88d0079e4b39abf60cee72971eede9ab553
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-12-01 23:36:47 -0500

    [2023/Rust/Day1] complete part 2

--- 2023/rust/src/day1/mod.rs
+++ 2023/rust/src/day1/mod.rs
@@ -2,6 +2,7 @@ use nom::{
     branch::alt,
     bytes::complete::{tag, take_till},
     character::complete::digit1,
+    combinator::map_res,
     IResult,
 };
 
@@ -13,15 +14,7 @@ pub fn part_1() {
             .flatten()
             .map(|line| {
                 let parsed = digits_part_1(line.as_str());
-
-                let mut parsed_characters = parsed.chars();
-                let first = parsed_characters.next();
-                if let (Some(first), Some(last)) = (first, parsed_characters.next_back().or(first))
-                {
-                    format!("{}{}", first, last).parse::<i32>().unwrap_or(0)
-                } else {
-                    0
-                }
+                extract_digits(parsed)
             })
             .sum();
 
@@ -34,16 +27,8 @@ pub fn part_2() {
         let result: i32 = lines
             .flatten()
             .map(|line| {
-                let mut parsed = digits_part_2(line.as_str());
-
-                let mut parsed_characters = parsed.chars();
-                let first = parsed_characters.next();
-                if let (Some(first), Some(last)) = (first, parsed_characters.next_back().or(first))
-                {
-                    format!("{}{}", first, last).parse::<i32>().unwrap_or(0)
-                } else {
-                    0
-                }
+                let parsed = digits_part_2(line.as_str());
+                extract_digits(parsed)
             })
             .sum();
 
@@ -83,9 +68,19 @@ fn digits_part_1(input: &str) -> String {
     parsed
 }
 
-fn word_digit(input: &str) -> IResult<&str, &str> {
-    alt((
-        tag::<_, &str, nom::error::Error<_>>("one"),
+fn make_word_digit_tags<'a>() -> (
+    impl Fn(&'a str) -> IResult<&str, &str>,
+    impl Fn(&'a str) -> IResult<&str, &str>,
+    impl Fn(&'a str) -> IResult<&str, &str>,
+    impl Fn(&'a str) -> IResult<&str, &str>,
+    impl Fn(&'a str) -> IResult<&str, &str>,
+    impl Fn(&'a str) -> IResult<&str, &str>,
+    impl Fn(&'a str) -> IResult<&str, &str>,
+    impl Fn(&'a str) -> IResult<&str, &str>,
+    impl Fn(&'a str) -> IResult<&str, &str>,
+) {
+    (
+        tag("one"),
         tag("two"),
         tag("three"),
         tag("four"),
@@ -94,12 +89,53 @@ fn word_digit(input: &str) -> IResult<&str, &str> {
         tag("seven"),
         tag("eight"),
         tag("nine"),
-        tag("nine"),
-    ))(input)
+    )
+}
+
+fn word_digit(input: &str) -> IResult<&str, &str> {
+    map_res(alt(make_word_digit_tags()), |res| match res {
+        "one" => Ok::<&str, nom::error::Error<&str>>("1"),
+        "two" => Ok("2"),
+        "three" => Ok("3"),
+        "four" => Ok("4"),
+        "five" => Ok("5"),
+        "six" => Ok("6"),
+        "seven" => Ok("7"),
+        "eight" => Ok("8"),
+        "nine" => Ok("9"),
+        _ => Ok("0"),
+    })(input)
 }
 
 fn digits_part_2(input: &str) -> String {
-    if let Ok((remaining, matched)) = word_digit(input) {}
+    let mut parsed = String::new();
+    let mut remaining = Some(input);
 
-    String::new()
+    while let Some(remaining_to_process) = remaining {
+        if let Ok((_, digit)) = word_digit(remaining_to_process) {
+            parsed.push_str(digit);
+            remaining = Some(&remaining_to_process[1..]);
+        } else if let Ok((next_remaining, digit)) =
+            digit1::<_, nom::error::Error<_>>(remaining_to_process)
+        {
+            parsed.push_str(digit);
+            remaining = Some(next_remaining);
+        } else if !remaining_to_process.is_empty() {
+            remaining = Some(&remaining_to_process[1..]);
+        } else {
+            remaining = None;
+        }
+    }
+
+    parsed
+}
+
+fn extract_digits(parsed: String) -> i32 {
+    let mut parsed_characters = parsed.chars();
+    let first = parsed_characters.next();
+    if let (Some(first), Some(last)) = (first, parsed_characters.next_back().or(first)) {
+        format!("{}{}", first, last).parse::<i32>().unwrap_or(0)
+    } else {
+        0
+    }
 }
