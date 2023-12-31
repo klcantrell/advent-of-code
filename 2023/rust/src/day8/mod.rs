1879289f33920da6ec1f6ff71829cab0fbd96b61
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-12-30 22:13:41 -0500

    [2023/Rust/Day8] complete part 1

---
+++ 2023/rust/src/day8/mod.rs
@@ -0,0 +1,70 @@
+use std::collections::HashMap;
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::alpha1,
+    combinator::{eof, iterator},
+    sequence::{delimited, separated_pair, terminated},
+    IResult,
+};
+
+type DesertMap<'a> = (&'a str, HashMap<String, (String, String)>);
+
+pub fn part_1() {
+    let input = include_str!("../../puzzle_inputs/day8.txt");
+    let (_, (directions, map)) = parse_desert_map(input).expect("Malformed desert map");
+
+    let mut step_count = 0;
+    let mut current_step = "AAA";
+
+    while current_step != "ZZZ" {
+        let direction = directions
+            .chars()
+            .nth(step_count % directions.len())
+            .expect("Not enough steps in puzzle input");
+
+        let crossroad = map
+            .get(current_step)
+            .unwrap_or_else(|| panic!("{} should be a valid destination", current_step));
+        current_step = if direction == 'L' {
+            &crossroad.0
+        } else {
+            &crossroad.1
+        };
+
+        step_count += 1;
+    }
+
+    println!("Day 8 Part 1 solution: {}", step_count);
+}
+
+fn parse_desert_map(input: &str) -> IResult<&str, DesertMap> {
+    let (map_input, directions) = terminated(alpha1, tag("\n\n"))(input)?;
+    let mut map: HashMap<String, (String, String)> = HashMap::new();
+
+    let mut map_rows = iterator(
+        map_input,
+        terminated(
+            separated_pair(
+                alpha1::<&str, nom::error::Error<&str>>,
+                tag(" = "),
+                delimited(
+                    nom::character::complete::char('('),
+                    separated_pair(alpha1, tag(", "), alpha1),
+                    nom::character::complete::char(')'),
+                ),
+            ),
+            alt((tag("\n"), eof)),
+        ),
+    );
+
+    map_rows.for_each(|map_row| {
+        map.insert(
+            map_row.0.to_string(),
+            (map_row.1 .0.to_string(), map_row.1 .1.to_string()),
+        );
+    });
+
+    Ok(("", (directions, map)))
+}
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -5,27 +5,30 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 mod utils;
 
 fn main() {
-    day1::part_1();
-    day1::part_2();
+    // day1::part_1();
+    // day1::part_2();
 
-    day2::part_1();
-    day2::part_2();
+    // day2::part_1();
+    // day2::part_2();
 
-    day3::part_1();
-    day3::part_2();
+    // day3::part_1();
+    // day3::part_2();
 
-    day4::part_1();
-    day4::part_2();
+    // day4::part_1();
+    // day4::part_2();
 
-    day5::part_1();
-    day5::part_2();
+    // day5::part_1();
+    // day5::part_2();
 
-    day6::part_1();
-    day6::part_2();
+    // day6::part_1();
+    // day6::part_2();
 
-    day7::part_1();
-    day7::part_2();
+    // day7::part_1();
+    // day7::part_2();
+
+    day8::part_1();
 }
