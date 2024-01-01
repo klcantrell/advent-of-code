b31c6e22ed32d0faee77b104d078360caadc78e5
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-12-31 20:44:05 -0500

    [2023/Rust/Day8] complete part 2

--- 2023/rust/src/day8/mod.rs
+++ 2023/rust/src/day8/mod.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::alpha1,
+    character::complete::{alpha1, alphanumeric1},
     combinator::{eof, iterator},
     sequence::{delimited, separated_pair, terminated},
     IResult,
@@ -39,6 +39,56 @@ pub fn part_1() {
     println!("Day 8 Part 1 solution: {}", step_count);
 }
 
+pub fn part_2() {
+    let input = include_str!("../../puzzle_inputs/day8.txt");
+    let (_, (directions, map)) = parse_desert_map(input).expect("Malformed desert map");
+
+    let ghost_nodes: Vec<String> = map
+        .iter()
+        .filter_map(|(node, _)| {
+            if node.ends_with('A') {
+                Some(node.to_string())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let initial_destinations: Vec<usize> = ghost_nodes
+        .iter()
+        .map(|node| {
+            let mut step_count = 0;
+            let mut current_step = node;
+
+            while !current_step.ends_with('Z') {
+                let direction = directions
+                    .chars()
+                    .nth(step_count % directions.len())
+                    .expect("Not enough steps in puzzle input");
+
+                let crossroad = map
+                    .get(current_step)
+                    .unwrap_or_else(|| panic!("{} should be a valid destination", current_step));
+                current_step = if direction == 'L' {
+                    &crossroad.0
+                } else {
+                    &crossroad.1
+                };
+
+                step_count += 1;
+            }
+
+            step_count
+        })
+        .collect();
+
+    let converged_destination = initial_destinations
+        .iter()
+        .fold(1, |d, current| num::integer::lcm(d, *current));
+
+    println!("Day 8 Part 2 solution: {}", converged_destination);
+}
+
 fn parse_desert_map(input: &str) -> IResult<&str, DesertMap> {
     let (map_input, directions) = terminated(alpha1, tag("\n\n"))(input)?;
     let mut map: HashMap<String, (String, String)> = HashMap::new();
@@ -47,11 +97,11 @@ fn parse_desert_map(input: &str) -> IResult<&str, DesertMap> {
         map_input,
         terminated(
             separated_pair(
-                alpha1::<&str, nom::error::Error<&str>>,
+                alphanumeric1::<&str, nom::error::Error<&str>>,
                 tag(" = "),
                 delimited(
                     nom::character::complete::char('('),
-                    separated_pair(alpha1, tag(", "), alpha1),
+                    separated_pair(alphanumeric1, tag(", "), alphanumeric1),
                     nom::character::complete::char(')'),
                 ),
             ),
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -9,26 +9,27 @@ mod day8;
 mod utils;
 
 fn main() {
-    // day1::part_1();
-    // day1::part_2();
+    day1::part_1();
+    day1::part_2();
 
-    // day2::part_1();
-    // day2::part_2();
+    day2::part_1();
+    day2::part_2();
 
-    // day3::part_1();
-    // day3::part_2();
+    day3::part_1();
+    day3::part_2();
 
-    // day4::part_1();
-    // day4::part_2();
+    day4::part_1();
+    day4::part_2();
 
-    // day5::part_1();
-    // day5::part_2();
+    day5::part_1();
+    day5::part_2();
 
-    // day6::part_1();
-    // day6::part_2();
+    day6::part_1();
+    day6::part_2();
 
-    // day7::part_1();
-    // day7::part_2();
+    day7::part_1();
+    day7::part_2();
 
     day8::part_1();
+    day8::part_2();
 }
